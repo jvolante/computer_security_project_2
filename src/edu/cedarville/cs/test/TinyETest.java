84c9b2a0752307d7bce7d6a6d35f83e75ba70e99
Jackson Volante <hikerjv@gmail.com>
2016-02-07 15:05:51 -0500

    Added stub classes for unit testing

---
+++ src/edu/cedarville/cs/test/TinyETest.java
@@ -0,0 +1,33 @@
+package edu.cedarville.cs.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jackson on 2/7/2016.
+ */
+public class TinyETest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+
+    }
+}
---
+++ src/edu/cedarville/cs/test/ToolsTest.java
@@ -0,0 +1,39 @@
+package edu.cedarville.cs.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jackson on 2/7/2016.
+ */
+public class ToolsTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvertFromBytesToInts() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvertFromHexStringToInts() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvertFromIntsToBytes() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvertFromIntsToHexString() throws Exception {
+
+    }
+}
